a1c87067bc593d16764d46c779204d7f1202069d
Coding in Flow <info@codinginflow.com>
2023-11-05 09:53:23 +0100

    Add fetch cleanup lesson

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,72 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    connect();
-    return () => disconnect();
-  }, []);
+  const [searchInput, setSearchInput] = useState("");
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    function handleKeyPress(e: KeyboardEvent) {
-      console.log("key pressed", e.key);
+    let ignore = false;
+    async function searchProducts() {
+      const products = await fetchProducts(searchInput);
+      if (!ignore) {
+        setProducts(products);
+      }
+    }
+    if (searchInput) {
+      searchProducts();
+    } else {
+      setProducts([]);
     }
 
-    window.addEventListener("keypress", handleKeyPress);
-    return () => window.removeEventListener("keypress", handleKeyPress);
-  }, []);
+    return () => {
+      ignore = true;
+    };
+  }, [searchInput]);
 
   return (
     <div className="p-4">
-      <p>Check the console for logs</p>
+      <input
+        type="text"
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+        placeholder="Search products..."
+        className="border border-gray-300 rounded-md p-2 w-full mb-4 max-w-sm"
+      />
+      <div>
+        {products.map((product) => (
+          <div className="flex items-center mb-1" key={product.id}>
+            <img
+              className="w-10 h-10 rounded mr-4"
+              src={product.thumbnail}
+              alt="Product thumbnail"
+            />
+            <p>{product.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-function connect() {
-  console.log("connecting...");
-  console.log("connected");
+async function fetchProducts(searchQuery: string): Promise<Product[]> {
+  await randomDelay();
+  const response = await fetch(
+    `https://dummyjson.com/products/search?q=${searchQuery}`
+  );
+  const data = await response.json();
+  return data.products;
 }
 
-function disconnect() {
-  console.log("disconnecting...");
-  console.log("disconnected");
+interface Product {
+  id: string;
+  title: string;
+  thumbnail: string;
+}
+
+async function randomDelay() {
+  const delay = Math.floor(Math.random() * 2000) + 1000;
+  return new Promise((resolve) => setTimeout(resolve, delay));
 }
